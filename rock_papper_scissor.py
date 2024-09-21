122f96d46b5460a947ee2045f9da2c9dd8a3e111
Malem Sanasam <sanasammalem78@gmail.com>
2024-09-21 20:21:17 +0530

    version 1

--- rock_papper_scissor.py
+++ rock_papper_scissor.py
@@ -1,6 +1,9 @@
 import random
 print("Rock Paper Scissors")
-print("Pick one: ")
+user_input = input("Pick one: ").lower().strip()
+if user_input == "rock" and user_input == "scissor":
+  print("You win🥳")
+  
 items = ("rock", "paper", "scissor")
 
 print(random.choice(items))
