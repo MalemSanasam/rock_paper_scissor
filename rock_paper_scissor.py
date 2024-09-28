ee403a3c29f7ab0a642c4f4ad7d752c9724da4d8
Malem Sanasam <sanasammalem78@gmail.com>
2024-09-28 15:02:46 +0530

    adding class

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,38 +1,40 @@
 import random
-def rps(user_input, computer):
-  # user picking rock
-  if user_input == "rock":
-    if computer == "scissor":
-      print("you win🥳")
-    elif computer == user_input:
-      print("Draw📍")  
-    else:
-      print("you lose😟")  
-  # user picking scissor    
-  if user_input == "scissor":
-    if computer == "paper":
-      print("you win🥳")
-    elif computer == user_input:
+class RockPaperScissor:
+  def rps(self, user_input, computer):
+    # user picking rock
+    if user_input == "rock":
+      if computer == "scissor":
+        print("you win🥳")
+      elif computer == user_input:
         print("Draw📍")  
-    else:
-      print("you lose😟") 
-  # user picking paper     
-  if user_input == "paper":
-    if computer == "rock":
-      print("you win🥳")
-    elif computer == user_input:
-        print("Draw📍")  
-    else: 
-      print("you lose😟")  
+      else:
+        print("you lose😟")  
+    # user picking scissor    
+    if user_input == "scissor":
+      if computer == "paper":
+        print("you win🥳")
+      elif computer == user_input:
+          print("Draw📍")  
+      else:
+        print("you lose😟") 
+    # user picking paper     
+    if user_input == "paper":
+      if computer == "rock":
+        print("you win🥳")
+      elif computer == user_input:
+          print("Draw📍")  
+      else: 
+        print("you lose😟")  
 def main():
+  game = RockPaperScissor()
   while True:
+    print("Wellcome to Rock Paper Scissors Game🎮")
     items = ("rock", "paper", "scissor")
     computer = random.choice(items)
     print("🪨  📃 ✂️")
-    print("Wellcome to Rock Paper Scissors Game🎮")
     user_input = input("You: ").lower().strip()
     print(f"Computer: {computer.capitalize()}") 
-    rps(user_input, computer)
+    game.rps(user_input, computer)
     exit_option = input("Do you want to continue to play?(y/n) ").lower().strip()
     if exit_option != "y":
       break
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+
+    items = ("rock", "paper", "scissor")
